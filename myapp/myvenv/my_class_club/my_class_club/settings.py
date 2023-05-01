f5f170dae5601378fd8ecffa7f6147def9aeba8e
Itu-Jose <itujose653@gmail.com>
2023-05-01 10:00:36 +0200

    Remove secrets from project

--- myapp/myvenv/my_class_club/my_class_club/settings.py
+++ myapp/myvenv/my_class_club/my_class_club/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-21g15iy)hs+f%k+xr0a@xph^cdj-=r%&eo6kyxjx3agnjt8jjh'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
